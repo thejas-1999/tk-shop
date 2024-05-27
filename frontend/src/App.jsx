66c5b24d1d39ebf7de68a6bae8ce17dd9fedaaba
thejas-1999 <thejask074@gmail.com>
2024-05-27 11:03:47 +0530

    add routing to the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+         <Outlet/>
         </Container>
         
       </main>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './assets/styles/index.css'
-/* import 'bootstrap/dist/css/bootstrap.min.css' */
-import './assets/styles/bootstrap.custom.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./assets/styles/index.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/styles/bootstrap.custom.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<HomeScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
