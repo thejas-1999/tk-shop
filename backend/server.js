b8fa7179d0d6e40dfb86d186c3b1046dbf7da015
thejas-1999 <thejask074@gmail.com>
2024-05-29 16:48:12 +0530

    create a new error handler method

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { notFound, errorHandler } from "./middleware/error.middleware.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import productRoutes from "./routes/product.routes.js";
@@ -14,6 +14,8 @@ const port = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 mongoose
   .connect(MONGO_URI)
