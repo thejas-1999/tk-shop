636d9c63575f3663e32aadeb8d7b26244199ab39
thejas-1999 <thejask074@gmail.com>
2024-05-30 14:35:28 +0530

    create a cartSlice for cart functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice";
+import cartSliceReducer from "../slices/cart.apiSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export default store;
