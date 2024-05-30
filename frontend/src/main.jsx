85ab995ffdf0d184adb5a4f32c8a86090dc4363c
thejas-1999 <thejask074@gmail.com>
2024-05-30 17:07:19 +0530

    create the front end of the cart page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,12 +14,14 @@ import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
 import { Provider } from "react-redux";
 import store from "./store";
+import CartScreen from "./screens/CartScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
