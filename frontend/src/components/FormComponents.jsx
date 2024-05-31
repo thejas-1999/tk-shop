03f9cc16a7cca11655f4af6440031e9f12af95a9
thejas-1999 <thejask074@gmail.com>
2024-05-31 15:09:13 +0530

    create a form component for login

---
+++ frontend/src/components/FormComponents.jsx
@@ -0,0 +1,16 @@
+import { Children } from 'react'
+import {Container,Row,Col} from 'react-bootstrap'
+
+const FormComponents = ({children}) => {
+  return (
+    <Container>
+        <Row className='justify-container-md-center'>
+            <Col xs={12} md={6}>
+                {Children}
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default FormComponents
