f08e40bfe0c45d69166867631b74dfb7f5a52979
thejas-1999 <thejask074@gmail.com>
2024-06-01 14:17:14 +0530

    create a private route for do not access the private pages without authendication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import App from "./App.jsx";
 import "./assets/styles/index.css";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/styles/bootstrap.custom.css";
 import HomeScreen from "./screens/HomeScreen.jsx";
@@ -27,7 +28,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
